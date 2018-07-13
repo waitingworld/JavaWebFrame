6c9d93772d3bc76debc04cbdeea3f6e91388db9a
waitingworld <707535808@qq.com>
2018-07-14 01:23:14 +0800

    整合angularAMD，实现页面的控制器按需加载

--- src/main/java/com/gs/controller/BaseController.java
+++ src/main/java/com/gs/controller/BaseController.java
@@ -3,7 +3,8 @@ package com.gs.controller;
 import com.alibaba.fastjson.JSONObject;
 import com.gs.model.City;
 import com.gs.service.BaseService;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,16 +17,15 @@ import java.util.List;
 @Controller
 @RequestMapping("/baseController")
 public class BaseController {
-    private Logger logger = Logger.getLogger(BaseController.class);
-
+    private static final Logger logger = LogManager.getLogger(BaseController.class);
     @Resource
     private BaseService baseService;
 
     @ResponseBody
     @RequestMapping("/selectAllCity")
     public List<City> selectAllCity(@RequestBody JSONObject data, HttpServletRequest request) {
-        logger.info("查询数据信息,data:" + data.toJSONString());
         List<City> cities = baseService.selectAllCity(data);
+        logger.info("selectAllCity:cities:{}",cities.size());
         return cities;
     }
 }
--- src/main/java/com/gs/service/impl/BaseServiceImpl.java
+++ src/main/java/com/gs/service/impl/BaseServiceImpl.java
@@ -4,7 +4,8 @@ import com.alibaba.fastjson.JSONObject;
 import com.gs.dao.BaseMapper;
 import com.gs.model.City;
 import com.gs.service.BaseService;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,12 +15,12 @@ import java.util.List;
 @Service(value = "baseService")
 @Transactional(rollbackFor = Exception.class)
 public class BaseServiceImpl implements BaseService {
-    private Logger logger = Logger.getLogger(BaseServiceImpl.class);
-
+    private static final Logger logger = LogManager.getLogger(BaseServiceImpl.class);
     @Autowired
     private BaseMapper baseMapper;
 
     public List<City> selectAllCity(JSONObject data) {
+        logger.info("{}:selectAllCity:{},", "BaseServiceImpl", data.toJSONString());
         List<City> result = baseMapper.selectAllCity(data);
         return result;
     }
