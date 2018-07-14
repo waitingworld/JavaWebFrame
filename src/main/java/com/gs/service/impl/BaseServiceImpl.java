5ddcac1fc988a08ca6ba20f14ff2f7d77042e47a
waitingworld <707535808@qq.com>
2018-07-14 17:50:47 +0800

    整合com.github.pagehelper.PageInterceptor，实现MyBatis的物理分页

--- src/main/java/com/gs/service/impl/BaseServiceImpl.java
+++ src/main/java/com/gs/service/impl/BaseServiceImpl.java
@@ -1,6 +1,7 @@
 package com.gs.service.impl;
 
 import com.alibaba.fastjson.JSONObject;
+import com.github.pagehelper.PageHelper;
 import com.gs.dao.BaseMapper;
 import com.gs.model.City;
 import com.gs.service.BaseService;
@@ -21,6 +22,7 @@ public class BaseServiceImpl implements BaseService {
 
     public List<City> selectAllCity(JSONObject data) {
         logger.debug("{}:selectAllCity:{},", "BaseServiceImpl", data.toJSONString());
+        PageHelper.startPage(1, 10);
         List<City> result = baseMapper.selectAllCity(data);
         return result;
     }
